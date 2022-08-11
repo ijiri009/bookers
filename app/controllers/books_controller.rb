72d7820d9897ae19b2d3c079806b0623ef3dae2d
ijiri009 <ijiri009@users.noreply.github.com>
2022-08-11 14:28:26 +0000

    提出課題２

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,48 @@
+class BooksController < ApplicationController
+
+  before_action :set_book, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to @book, notice: 'Book was successfully created.'
+    else
+      @books = Book.all
+      render :index
+    end
+  end
+
+  def update
+    if @book.update(book_params)
+      redirect_to @book, notice: 'Book was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @book.destroy
+    redirect_to books_url
+  end
+
+  private
+    def set_book
+      @book = Book.find(params[:id])
+    end
+
+    def book_params
+      params.require(:book).permit(:title, :body)
+    end
+end
+
